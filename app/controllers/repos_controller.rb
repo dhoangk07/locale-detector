ecdbad99b33e8fa30247eb37a07cc73001d382ea
HoangDinh <dhoangk07@gmail.com>
2018-11-04 16:24:05 +0700

    Add ban to create new repo

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,2 +1,40 @@
 class ReposController < ApplicationController
+  # before_action :set_repos, only: %i[edit update show destroy]
+  def index
+    @repos = Repo.all
+  end
+  
+  def new
+    @repo = Repo.new
+  end
+  
+  def create
+    @repo = Repo.new(repo_params)
+    if @repo.save
+      redirect_to repos_path
+    else
+      flash.now[:error] = @repo.errors.full_messages.join(", ")
+    end
+  end 
+  
+  # def edit
+  # end
+  
+  # def update
+  # end
+  
+  # def show
+  # end
+  
+  # def destroy
+  # end
+  
+  private
+    def set_repo
+      @repo = Repo.find(params[:id])
+    end
+    
+    def repo_params
+      params.require(:repo).permit(:url, :name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :repos
 
   root to: 'welcomes#index'
 end
