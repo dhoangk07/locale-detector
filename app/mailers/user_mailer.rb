26d67f52c1df3529012954bd913f0be990c9f0a9
HoangDinh <dhoangk07@gmail.com>
2018-11-06 16:22:47 +0700

    Send email for owner of repo when missing key on locale

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,12 @@
 class UserMailer < ApplicationMailer
    default from: 'notifications@example.com'
  
-  def notify_missing_key_on_locale
-    @user = params[:user]
+  def notify_missing_key_on_locale_for_owner(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Notify Missing Key on Locale')
+  end
+
   def notify_missing_key_on_locale_for_subscribe(user)
     @user = user
     @url  = 'http://example.com/login'
