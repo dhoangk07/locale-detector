e1a304806d25f3b43f96f47ae3454a4a3b868d7f
HoangDinh <dhoangk07@gmail.com>
2018-11-15 16:59:40 +0700

    Change parameter for method perform

--- app/workers/run_clone_compare.rb
+++ app/workers/run_clone_compare.rb
@@ -1,7 +1,8 @@
 class RunCloneCompare
   @queue = :repo_queue
 
-  def self.perform(repo)
+  def self.perform(repo_id)
+    repo = Repo.find(repo_id)
     repo.run_clone
     repo.run_compare
   end
