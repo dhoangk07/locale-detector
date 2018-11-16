9b2831665d9cf3f288b79a9f2149bc1ea369b3d8
HoangDinh <dhoangk07@gmail.com>
2018-11-16 19:06:50 +0700

    Use a different Rails environment for worker

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,9 @@ set :repo_url, "https://github.com/dhoangk07/locale-detector.git"
 set :passenger_restart_with_touch, true
 set :deploy_to, "/home/deploy/locale-detector"
 
-set :workers, { "clone_queue" => 1, "delete_folder_queue" => 1, "fetch_description_queue" => 1 }
+# set :workers, { "clone_queue" => 1, "delete_folder_queue" => 1, "fetch_description_queue" => 1 }
 set :resque_environment_task, true
+set :resque_rails_env, "clone_queue", "delete_folder_queue", "fetch_description_queue"
 after "deploy:restart", "resque:restart"
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
