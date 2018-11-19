8ce446c342459086f57f2290d15d447ac11c236f
HoangDinh <dhoangk07@gmail.com>
2018-11-19 15:34:45 +0700

    Add ‘repo’ params for method of user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,18 @@
 class UserMailer < ApplicationMailer
    default from: 'notifications@example.com'
  
-  def notify_missing_key_on_locale_for_owner(user)
+  def notify_missing_key_on_locale_for_owner(user, repo)
     @user = user
+    @repo = repo
     @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Notify Missing Key on Locale')
   end
 
-  def notify_missing_key_on_locale_for_subscribe(user)
+  def notify_missing_key_on_locale_for_subscribe(user, repo)
     @user = user
+    @repo = repo
     @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Notify Missing Key on Locale')
   end
 end
+  
