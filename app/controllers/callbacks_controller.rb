07bb44c4808ddbacd75faf4a040737e4919b3fd5
HoangDinh <dhoangk07@gmail.com>
2018-11-20 11:59:05 +0700

    Recorrect callbacks_controller

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,10 +1,6 @@
-class CallbacksController < ApplicationController
+class CallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in_and_redirect @user
-  end
-
-  def method_name
-    
   end
 end
