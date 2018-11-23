c44506b46b63b02b4532ba3347ecbc08a1fbe77a
HoangDinh <dhoangk07@gmail.com>
2018-11-24 00:18:30 +0700

    Delete debugger on resque.rb

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,8 +1,5 @@
 require 'resque-scheduler'
 require 'resque/scheduler/server'
 Resque.redis = 'localhost:6379'
-require 'byebug'
-debugger
 Resque::Scheduler.dynamic = true
-
 Resque.schedule = YAML.load_file('config/resque_schedule.yml')
