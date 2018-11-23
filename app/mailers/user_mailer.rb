8b8fa309f950568c04dd520962d1d051ea4354ef
HoangDinh <dhoangk07@gmail.com>
2018-11-23 18:18:34 +0700

    Try to deploy cap

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,14 +4,14 @@ class UserMailer < ApplicationMailer
   def notify_missing_key_on_locale_for_owner(user, repo)
     @user = user
     @repo = repo
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Notify Missing Key on Locale')
-    # mail(to: @user.email, subject: "Welcome!").tap do |message|
-    #   message.mailgun_options = {
-    #     "tag" => ["abtest-option-a", "beta-user"],
-    #     "tracking-opens" => true,
-    #     "tracking-clicks" => "htmlonly"
-    #   }
+    # @url  = 'http://example.com/login'
+    # mail(to: @user.email, subject: 'Notify Missing Key on Locale')
+    mail(to: @user.email, subject: "Welcome!").tap do |message|
+      message.mailgun_options = {
+        "tag" => ["abtest-option-a", "beta-user"],
+        "tracking-opens" => true,
+        "tracking-clicks" => "htmlonly"
+      }
     end
   end
 
