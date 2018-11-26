143cc1ee2adc75f8068d798703815f8d527d6db3
HoangDinh <dhoangk07@gmail.com>
2018-11-26 19:04:22 +0700

    Add schema table for locale_key

--- app/models/locale_key.rb
+++ app/models/locale_key.rb
@@ -1,3 +1,11 @@
 class LocaleKey < ApplicationRecord
+  # Schema of LocaleKeys table
+  #   t.string "locale"
+  #   t.string "key"
+  #   t.string "value"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  #   t.integer "repo_id"
+  
   belongs_to :repo
 end
