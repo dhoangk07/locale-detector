dd9ebea8f228d77af9c8add9e69c5d8e8ed13ed7
HoangDinh <dhoangk07@gmail.com>
2018-11-27 21:12:10 +0700

    Delete comment on run_compare method

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -105,13 +105,10 @@ class Repo < ApplicationRecord
   def run_compare
     run_compare_yml_file
     en_keys = LocaleKey.where(repo_id: self.id, locale: 'en').distinct.pluck(:key)
-    # base_keys = LocaleKey.where(repo_id: self.id).distinct
-    # en_keys = base_keys.where(locale: 'en').pluck(:key)
     locale_lists = locale_keys.select('locale').where.not(locale: 'en').distinct 
     hash = {}
     locale_lists.each do |locale| 
       keys = LocaleKey.where(repo_id: self.id, locale: locale.locale.remove('.yml')).distinct.pluck(:key)
-      # keys = base_keys.where(locale: locale.locale.remove('.yml'))
       different_keys = en_keys - keys
       hash[locale.locale] = different_keys if different_keys != [] 
     end
