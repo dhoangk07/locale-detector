bbb4f54bcc955640825cbc0ba19b5cbd6fecbae6
HoangDinh <dhoangk07@gmail.com>
2018-12-03 07:56:47 +0700

    Delete comment on UserMailer for config mailgun

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,13 +6,6 @@ class UserMailer < ApplicationMailer
     @repo = repo
     @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Notify Missing Key on Locale')
-    # mail(to: @user.email, subject: "Welcome!").tap do |message|
-    #   message.mailgun_options = {
-    #     "tag" => ["abtest-option-a", "beta-user"],
-    #     "tracking-opens" => true,
-    #     "tracking-clicks" => "htmlonly"
-    #   }
-    # end
   end
 
   def notify_missing_key_on_locale_for_subscribe(user, repo)
